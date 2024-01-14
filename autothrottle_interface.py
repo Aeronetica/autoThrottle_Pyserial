bde09b33658012f6b3cbc0b81e42e4f74e14588c
loydhook <loyd-hook@utulsa.edu>
2024-01-13 21:25:56 -0600

    FIrst working writer

--- autothrottle_interface.py
+++ autothrottle_interface.py
@@ -1,1 +1,20 @@
 print ("Hello AutoThrottle")
+
+import serial
+import time
+ser = serial.Serial("COM3", 38400, timeout=1)
+h = 95
+i = 22
+print("write")
+ser.write(h)
+time.sleep(.1)
+ser.write(i)
+print("done")
+# time.sleep(.1)
+
+# # Read line   
+# while True:
+#     # Send character 'S' to start the program
+    
+
+# print("Done")
