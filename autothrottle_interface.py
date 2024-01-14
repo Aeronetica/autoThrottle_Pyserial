6f3a9b5ac9ae08670110ab46858be939000b990d
loydhook <loyd-hook@utulsa.edu>
2024-01-14 10:05:00 -0600

    Excercise both ports in one program... Cool

--- autothrottle_interface.py
+++ autothrottle_interface.py
@@ -2,16 +2,17 @@ print ("Hello AutoThrottle")
 
 import serial
 import time
-ser = serial.Serial("COM3", 38400, timeout=1)
+ser3 = serial.Serial("COM3", 38400, timeout=1)
+ser4 = serial.Serial("COM4", 38400, timeout=1)
 # h = 95
 # i = 22
 #Can we do read/write with the same one?
 for i in range(0, 100):
-    print("write")
-    ser.write(b"hello from COM 3")
-    print("read")
-    print(ser.read(5))
-
+    ser3.write(b"hello from COM 3")
+    ser4.write(b"hello from COM 4")
+    print("COM3: " + str(ser3.read(100)))
+    print("COM4: " + str(ser4.read(100)))
+    time.sleep(1)
 print("done")
 # time.sleep(.1)
 
