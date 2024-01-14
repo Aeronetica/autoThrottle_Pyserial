ea5b8452c3f516cd8df2c96c36f7c0fb84964e96
loydhook <loyd-hook@utulsa.edu>
2024-01-14 09:03:53 -0600

    change to text

--- autothrottle_interface.py
+++ autothrottle_interface.py
@@ -3,12 +3,10 @@ print ("Hello AutoThrottle")
 import serial
 import time
 ser = serial.Serial("COM3", 38400, timeout=1)
-h = 95
-i = 22
+# h = 95
+# i = 22
 print("write")
-ser.write(h)
-time.sleep(.1)
-ser.write(i)
+ser.write(b"hello")
 print("done")
 # time.sleep(.1)
 
