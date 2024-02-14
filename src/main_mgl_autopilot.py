c45887db3e7dcea438bf0e469f2658789be572ae
loydhook <loyd-hook@utulsa.edu>
2024-02-14 13:39:29 -0600

    changed for my box

--- src/main_mgl_autopilot.py
+++ src/main_mgl_autopilot.py
@@ -3,7 +3,7 @@ from throttle_controller import ThrottleController
 print("Hello AutoThrottle")
 
 the_throttle_controller = ThrottleController(
-    port_string="/dev/ttyUSB4",
+    port_string="/dev/ttyUSB0",
     config_file="./cfg/throttle_config.json",
     servo_number=1,
 )
